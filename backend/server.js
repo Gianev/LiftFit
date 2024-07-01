121e50b0413e6f7c346ed4bf7e359208b7c86e76
Giancarlo Evacula <giancarloe1203@gmail.com>
2024-07-01 18:43:12 -0400

    Updated Routing

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,19 @@
-const express = require('express')
+require('dotenv').config()
 
+//USE COMMAND "nodemon server.js" to run the server
+const express = require('express')
 
 //Creates the express APP
 const app = express()
 
+//Responds to get requests through routing
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome'})
+})
+
+// Listen to the requests
+app.listen(process.env.PORT, () =>{
+    console.log('listening on port 4000')
+})
+
+process.env
