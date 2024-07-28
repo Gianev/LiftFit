6679ceeb20e5f61aeb979c08ceb1d10fd0c97698
Giancarlo Evacula <giancarloe1203@gmail.com>
2024-07-28 19:00:46 -0400

    CSS

--- frontend/src/context/ThemeContext.js
+++ frontend/src/context/ThemeContext.js
@@ -1,8 +1,11 @@
 import { createContext, useState } from "react";
-
+import { useEffect } from "react";
 export const ThemeContext = createContext();
 export const ThemeContextProvider  = ({children}) =>{
     const [theme, setTheme] = useState('light');
+    useEffect(() => {
+        document.body.className = "body"+(theme === 'light' ? 'dark':'light');
+      }, [theme]);
     return(
     <ThemeContext.Provider value = {[theme, setTheme]}>
         {children}
